6fd7161fadd5617b4e82f73d60ce52fd7fc08885
Shemaremy <remyshema20@gmail.com>
2024-08-09 15:21:12 +0200

    Designed test form

--- Authenticator/src/App.jsx
+++ Authenticator/src/App.jsx
@@ -3,26 +3,126 @@ import './App.css'
 
 function App() {
 
+
+  
+
+  const [errors, setErrors] = useState({});
+
   const [isLogin, setIsLogin] = useState(true);
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showPasswordOne, setShowPasswordOne] = useState(false);
+  const [showPasswordTwo, setShowPasswordTwo] = useState(false);
+
+  const [passwordOne, setPasswordOne] = useState('');
+  const [passwordTwo, setPasswordTwo] = useState('');
+
+
 
 
   const handleFormChange = () => {
     setIsLogin(!isLogin);
   };
 
+  const handleHidePassword = () => {
+    setShowPassword(!showPassword)
+  }
+
+
+  const handleHidePasswordOne = () => {
+    setShowPasswordOne(!showPasswordOne);
+  }
+
+  const handleHidePasswordTwo = () => {
+    setShowPasswordTwo(!showPasswordTwo);
+  }
+
+
+
+
+  const validateForm = () => {
+
+    const newErrors = {};
+
+    const BorderOne = document.querySelector('.rimwe');
+    const BorderTwo = document.querySelector('.kabiri');
+
+    if(passwordOne !== passwordTwo) {
+      newErrors.passwordTwo = 'Your passwords must match sir!';
+      BorderOne.style.borderColor = 'red';
+      BorderTwo.style.borderColor = 'red';
+    }
+
+    return newErrors;
+  };
+
+
+  const handleSignUpSubmit = (e) => {
+    e.preventDefault();
+    const newErrors = validateForm();
+    const BorderOne = document.querySelector('.rimwe');
+    const BorderTwo = document.querySelector('.kabiri');
+    if (Object.keys(newErrors).length > 0) {
+        setErrors(newErrors);
+    } else {
+        // navigate('/');
+        // window.location.reload();
+        setErrors({});
+        BorderOne.style.borderColor = '';
+        BorderTwo.style.borderColor = '';
+        alert('Success');
+    }
+  }
+
+
+  const handlePasswordOneChange = (e) => {
+    const value = e.target.value;
+    setPasswordOne(value);
+  }
+
+  const handlePasswordTwoChange = (e) => {
+    const value = e.target.value;
+    setPasswordTwo(value);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   const LoginForm = (
     <form action="" className='login_form'>
       <div className='one'>
-        <p className='indicator'>Email</p>
+        <p className='indicator'>Email / Username</p>
         <div className='input_container'>
-          <input type="text" placeholder='Enter your username or email' name='Name'/>
+          <input type="text" 
+            placeholder='Enter your username or email' 
+            name='Email'
+            maxLength={25}
+            required
+          />
         </div>
       </div>
-      <div className='two'>
+      <div className='two '>
         <p className='indicator'>Password</p>
-        <div className='input_container'>
-          <input type="text" placeholder='Enter your password' name='Email'/>
+        <div className='input_container for-password'>
+          <input type={showPassword ? "text" : "password"} 
+            placeholder='Enter your password' 
+            name='Password'
+            maxLength={20}
+            required
+          />
+          <div className='eye-container' type="button" onClick={handleHidePassword}>
+            {showPassword ? <i className="fa-regular fa-eye-slash"></i> : <i className="fa-regular fa-eye"></i>}
+          </div>
         </div>
       </div>
       <div className='three'>
@@ -32,33 +132,62 @@ function App() {
   );
 
   const SignUpForm = (
-    <form action="" className='Sign-up-form'>
+    <form action="" className='Sign-up-form' onSubmit={handleSignUpSubmit}>
       <div className='one'>
         <p className='indicator'>Username</p>
         <div className='input_container'>
-          <input type="text" placeholder='Enter a preferred username' name='Name'/>
+          <input type="text" 
+            placeholder='Enter a preferred username' 
+            name='Name'
+            required
+          />
         </div>
       </div>
       <div className='two'>
         <p className='indicator'>Email</p>
         <div className='input_container'>
-          <input type="text" placeholder='Enter your email address' name='Email'/>
+          <input type="text" 
+            placeholder='Enter your email address' 
+            name='Email'
+            required
+          />
         </div>
       </div>
       <div className='two-a'>
         <p className='indicator'>Password</p>
-        <div className='input_container'>
-          <input type="text" placeholder='Enter password' name='Password'/>
+        <div className='input_container for-password rimwe'>
+          <input type={showPasswordOne ? "text" : "password"} 
+            placeholder='Enter password' 
+            name='FirstPassword'
+            maxLength={20}
+            value={passwordOne}
+            onChange={handlePasswordOneChange}
+            required
+          />
+          <div className='eye-container' type="button" onClick={handleHidePasswordOne}>
+            {showPasswordOne ? <i className="fa-regular fa-eye-slash"></i> : <i className="fa-regular fa-eye"></i>}
+          </div>
         </div>
       </div>
       <div className='two-b'>
         <p className='indicator'>Confirm Password</p>
-        <div className='input_container'>
-          <input type="text" placeholder='Confirm password' name='ConfPassword'/>
+        <div className='input_container for-password kabiri'>
+          <input type={showPasswordTwo ? "text" : "password"} 
+            placeholder='Confirm password' 
+            name='ConfPassword'
+            maxLength={20}
+            value={passwordTwo}
+            onChange={handlePasswordTwoChange}
+            required
+          />
+          <div className='eye-container' type="button" onClick={handleHidePasswordTwo}>
+            {showPasswordTwo ? <i className="fa-regular fa-eye-slash"></i> : <i className="fa-regular fa-eye"></i>}
+          </div>
         </div>
+        {errors.passwordTwo && <p className='error'>{errors.passwordTwo}</p>}
       </div>
       <div className='three'>
-        <button>Sign up</button>
+        <button type='submit'>Sign up</button>
       </div>
     </form>
   );
